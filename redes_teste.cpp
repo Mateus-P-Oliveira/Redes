78b210549290e9b852f24e582bf77ef63e57b62f
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-10-28 16:13:38 +0000

    Created Client Struct

--- redes_teste.cpp
+++ redes_teste.cpp
@@ -1,3 +1,21 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include <stdlib.h> 
+
+using namespace std;
+
+struct client{
+    string name;
+    int token_time, data_time;
+    string right_ip; 
+
+};
 
 
+int main(void){
+
+
+
+
+return 0;
+}
