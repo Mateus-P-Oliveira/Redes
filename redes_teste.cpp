7bcc0888d98485e0a3a40bbc0215361a1d60d959
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-10-29 16:18:03 +0000

    Classe Pronta

--- redes_teste.cpp
+++ redes_teste.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
+#include <stdlib.h>
 #include <string>
-#include <stdlib.h> 
 
 using namespace std;
 
-struct client{
-    string name;
-    int token_time, data_time;
-    string right_ip; 
-
+struct MachineClient {
+  string name;
+  int token_count;
+  string right_ip;
+  bool Generated_token;
 };
 
+MachineClient machine_creation() { // Mudar depois caso necessario
+  MachineClient Machine;
+  cin >> Machine.right_ip;
+  cin >> Machine.name;
+  cin >> Machine.token_count;
+  cin >> Machine.Generated_token;
 
-int main(void){
+  return Machine;
+}
+
+int main(void) {
+  MachineClient my_pc;
+  my_pc = machine_creation();
+  cout << my_pc.right_ip << endl;
+  cout << my_pc.name << endl;
+  cout << my_pc.token_count << endl;
+  cout << my_pc.Generated_token << endl;
 
 
 
-
-return 0;
+  return 0;
 }
