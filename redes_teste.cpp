dcef8dab631665b616ba618eb0136c27aa951160
Mateus-P-Oliveira <mateus.pinto@acad.pucrs.br>
2023-10-30 22:24:05 +0000

    Classe Client

--- redes_teste.cpp
+++ redes_teste.cpp
@@ -1,26 +1,25 @@
-#include <iostream>
 #include <arpa/inet.h>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <stdlib.h>
 #include <string>
-#include <bits/stdc++.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-
-#define PORT     8080  //Isso vai ter que ser definido depois pelo txt
-#define MAXLINE 1024 
+#define PORT 8080 // Isso vai ter que ser definido depois pelo txt
+#define MAXLINE 1024
 
 using namespace std;
 
-struct sockaddr_in sa; //Address
+struct sockaddr_in sa; // Address
 
-//https://www.geeksforgeeks.org/udp-server-client-implementation-c/ - Como Implementar UDP
+// https://www.geeksforgeeks.org/udp-server-client-implementation-c/ - Como
+// Implementar UDP
 struct MachineClient {
   string name;
-  int token_count,right_ip_data;
+  int token_count, right_ip_data;
   string right_ip;
   bool Generated_token;
 };
@@ -31,8 +30,42 @@ MachineClient machine_creation() { // Mudar depois caso necessario
   cin >> Machine.name;
   cin >> Machine.token_count;
   cin >> Machine.Generated_token;
-  inet_pton(AF_INET, Machine.right_ip.c_str(), &(sa.sin_addr)); //Converte de string para ip
+  inet_pton(AF_INET, Machine.right_ip.c_str(),
+            &(sa.sin_addr)); // Converte de string para ip
   return Machine;
+}
+
+void client() {
+  int sockfd;
+  char buffer[MAXLINE];
+  const char *hello = "Hello from client";
+
+  // Creating socket file descriptor
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("socket creation failed");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&sa, 0, sizeof(sa));
+
+  // Filling server information
+  sa.sin_family = AF_INET;
+  sa.sin_port = htons(PORT); // Aqui salvo a porta do arquivo de configuração
+  sa.sin_addr.s_addr = INADDR_ANY; // Aqui salvo o IP do PC a direita
+
+  int n;
+  socklen_t len;
+
+  sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM,
+         (const struct sockaddr *)&sa, sizeof(sa));
+  std::cout << "Hello message sent." << std::endl;
+
+  n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL,
+               (struct sockaddr *)&sa, &len);
+  buffer[n] = '\0';
+  std::cout << "Server :" << buffer << std::endl;
+
+  close(sockfd);
 }
 
 int main(void) {
@@ -42,47 +75,10 @@ int main(void) {
   cout << my_pc.name << endl;
   cout << my_pc.token_count << endl;
   cout << my_pc.Generated_token << endl;
+  //-----------------------------------Servidor
 
-  //-----------------------------------
-    int sockfd; 
-    char buffer[MAXLINE]; 
-    const char *hello = "Hello from client"; 
-   
-   
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-   
-    memset(&sa, 0, sizeof(sa)); 
-       
-    // Filling server information 
-    sa.sin_family = AF_INET; 
-    sa.sin_port = htons(PORT); 
-    sa.sin_addr.s_addr = INADDR_ANY; 
-       
-    int n;
-    socklen_t len; 
-       
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &sa,  
-            sizeof(sa)); 
-    std::cout<<"Hello message sent."<<std::endl; 
-           
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                MSG_WAITALL, (struct sockaddr *) &sa, 
-                &len); 
-    buffer[n] = '\0'; 
-    std::cout<<"Server :"<<buffer<<std::endl; 
-   
-    close(sockfd); 
-    
-
-
-
-
-
+  //----------------------------------- Cliente
+  client();
   //---------------------------------
   return 0;
 }
